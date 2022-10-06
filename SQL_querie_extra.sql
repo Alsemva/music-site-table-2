36bb651eafb43337ac179d651f7a3e657e0512cf
Alexey Shorokhov <alsemva@yandex.ru>
2022-10-06 11:15:06 +0600

    Add department column

--- SQL_querie_extra.sql
+++ SQL_querie_extra.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS Employee (
 	id_employee SERIAL PRIMARY KEY,
 	name_employee VARCHAR(40) NOT NULL,
+	name_department VARCHAR(40) NOT NULL,
 	supervisor_id int,
-	FOREIGN key (supervisor_id) REFERENCES Employee(id_employee)
+	FOREIGN KEY (supervisor_id) REFERENCES Employee(id_employee)
 );
